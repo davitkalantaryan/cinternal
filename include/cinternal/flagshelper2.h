99c3df81183f9e74ad214e47a0475075a8e058f6
DAVIT Kalantaryan <Davit Kalantaryan>
2023-03-29 00:57:09 +0200

    added flags helper version 2

--- include/cinternal/flagshelper2.h
+++ include/cinternal/flagshelper2.h
@@ -7,21 +7,12 @@
 // if CPPUTILS_FLAGS_UN_RAW macros is used, then for uint64_t include #include <stdint.h>
 //
 
-#ifndef CINTERNAL_INCLUDE_CINTERNAL_FLAGSHELPER_H
-#define CINTERNAL_INCLUDE_CINTERNAL_FLAGSHELPER_H
+#ifndef CINTERNAL_INCLUDE_CINTERNAL_FLAGSHELPER2_H
+#define CINTERNAL_INCLUDE_CINTERNAL_FLAGSHELPER2_H
 
 #include <cinternal/internal_header.h>
-
-#ifdef USING_CINTERNA_FLAGS_HELPER_VERSION2
-
-#include <cinternal/flagshelper2.h>
-
-#else
-
 #include <cinternal/macroses02.h>
 
-#define CPPUTILS_MAKE_BITS_POSITIVE     1
-#define CPPUTILS_MAKE_BITS_NEGATIVE     2
 #define CPPUTILS_MAKE_BITS_TRUE         1
 #define CPPUTILS_MAKE_BITS_FALSE        2
 #define CPPUTILS_INIT_BITS              0xaaaaaaaaaaaaaaaa
@@ -35,7 +26,7 @@
 
 
 #define CPPUTILS_BOTH_BITS_RAW(_type,_name) \
-    _type  _name ## _both : 2
+    _type  _name  : 2
 #define CPPUTILS_BOTH_BITS(_name)  CPPUTILS_BOTH_BITS_RAW(uint64_t,_name)
 #define CPPUTILS_BOTH_BITS2(_a,_name)  CPPUTILS_BOTH_BITS(_name) ;
 
@@ -46,22 +37,19 @@
 
 #define CPPUTILS_FLAGS_UN_RAW(_name, _numberOfReserved,...)  \
     union _name {  \
-        uint64_t  all; \
+        uint64_t  wr_all; \
         struct{ \
             CPPUTILS_BITS_B(__VA_ARGS__)                \
             uint64_t    reserved01 : _numberOfReserved; \
-        }b; \
+        }rd; \
         struct{ \
             CPPUTILS_BITS_B2(__VA_ARGS__)               \
             uint64_t    reserved01 : _numberOfReserved; \
-        }b2;    \
+        }wr;    \
     }
 
 #define CPPUTILS_FLAGS_UN(...)    CPPUTILS_FLAGS_UN_RAW(,64-2*CPPUTILS_NARGS(__VA_ARGS__),__VA_ARGS__)
 #define CPPUTILS_FLAGS_UN_NM(_name,...)    CPPUTILS_FLAGS_UN_RAW(_name,64-2*CPPUTILS_NARGS(__VA_ARGS__),__VA_ARGS__)
 
 
-#endif  //  #ifdef USING_CINTERNA_FLAGS_HELPER_VERSION2
-
-
-#endif  // #ifndef CINTERNAL_INCLUDE_CINTERNAL_FLAGSHELPER_H
+#endif  // #ifndef CINTERNAL_INCLUDE_CINTERNAL_FLAGSHELPER2_H
