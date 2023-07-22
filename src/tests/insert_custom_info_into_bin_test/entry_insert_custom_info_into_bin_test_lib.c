62d9e2df1050cb342b5439899289d8cd8682faca
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-22 23:10:58 +0200

    added some comments to the file src/tests/insert_custom_info_into_bin_test/entry_insert_custom_info_into_bin_test_lib.c

--- src/tests/insert_custom_info_into_bin_test/entry_insert_custom_info_into_bin_test_lib.c
+++ src/tests/insert_custom_info_into_bin_test/entry_insert_custom_info_into_bin_test_lib.c
@@ -1,4 +1,5 @@
 //
+// repo:			cinternal
 // file:			entry_insert_custom_info_into_bin_test_lib.c
 // path:			src/tests/insert_custom_info_into_bin_test/entry_insert_custom_info_into_bin_test_lib.cpp
 // created on:		2023 Feb 27
