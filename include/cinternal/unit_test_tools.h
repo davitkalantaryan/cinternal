f2ebbe65c581f17444b4237fc4edd35887d2d13d
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-23 17:53:33 +0200

    trying to resolve name aliasing for clang

--- include/cinternal/unit_test_tools.h
+++ include/cinternal/unit_test_tools.h
@@ -21,22 +21,26 @@ CPPUTILS_BEGIN_C
 
 typedef void (*TypeFunction)(void);
 
+// if you need to have tests after main called, then overrwrite below 2 functions
 CPPUTILS_DLL_PRIVATE void CinternalAddUnitTestFunction(TypeFunction a_function);
 CPPUTILS_DLL_PRIVATE void CinternalIterateAndCallUnitTestFunctions(void);
 
+
+
+CPPUTILS_DLL_PRIVATE void CallCinternalAddUnitTestFunction(TypeFunction a_function);
 
 #define CPPUTILS_UTEST_BEFORE_MAIN(_maj,_min)	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_bm_ ## _maj ## _min)
 
 #define CPPUTILS_UTEST(_maj,_min)	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT;	\
 	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_adder_ ## _maj ## _min){						\
-		CinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min);					\
+		CallCinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min);				\
 	}																							\
 	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT
 
 
 #define CPPUTILS_UTEST_ALL(_maj,_min)	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT;\
 	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_adder_ ## _maj ## _min){						\
-		CinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min);					\
+		CallCinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min);				\
 		cinternal_unit_test_ ## _maj ## _min ();												\
 	}																							\
 	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT 
--- src/core/cinternal_core_unit_test_main.c
+++ src/core/cinternal_core_unit_test_main.c
@@ -12,9 +12,12 @@
 CPPUTILS_BEGIN_C
 
 
+static void CallCinternalIterateAndCallUnitTestFunctions(void);
+
 int main(void)
 {
-	CinternalIterateAndCallUnitTestFunctions();
+	//CinternalIterateAndCallUnitTestFunctions();
+    CallCinternalIterateAndCallUnitTestFunctions();
 	return 0;
 }
 
@@ -36,16 +39,49 @@ CPPUTILS_DLL_PRIVATE void CinternalIterateAndCallUnitTestFunctions_alternate(voi
 #pragma comment(linker, "/alternatename:" CPPUTILS_FNAME_PREFIX "CinternalIterateAndCallUnitTestFunctions=" CPPUTILS_FNAME_PREFIX "CinternalIterateAndCallUnitTestFunctions_alternate")
 #else
 #ifdef CPPUTILS_FNAME_PREFIX_HAS_UNDERLINE
+
+#define HAS_ACTIVE_CALL
+extern CPPUTILS_DLL_PRIVATE void CinternalAddUnitTestFunction(TypeFunction a_function) __attribute__((weak_import));
+extern CPPUTILS_DLL_PRIVATE void CinternalIterateAndCallUnitTestFunctions(void) __attribute__((weak_import));
+
 //#pragma clang attribute push ([[weak]], _CinternalAddUnitTestFunction = CinternalAddUnitTestFunction_alternate)
 //#pragma weak _CinternalAddUnitTestFunction=CinternalAddUnitTestFunction_alternate
 //#pragma weak _CinternalIterateAndCallUnitTestFunctions=CinternalIterateAndCallUnitTestFunctions_alternate
-CPPUTILS_DLL_PRIVATE void CinternalAddUnitTestFunction(TypeFunction a_function) __attribute__((weak, alias("CinternalAddUnitTestFunction_alternate")));
-CPPUTILS_DLL_PRIVATE void CinternalIterateAndCallUnitTestFunctions(void) __attribute__((weak, alias("CinternalIterateAndCallUnitTestFunctions_alternate")));
+//CPPUTILS_DLL_PRIVATE void CinternalAddUnitTestFunction(TypeFunction a_function) __attribute__((weak, alias("CinternalAddUnitTestFunction_alternate")));
+//CPPUTILS_DLL_PRIVATE void CinternalIterateAndCallUnitTestFunctions(void) __attribute__((weak, alias("CinternalIterateAndCallUnitTestFunctions_alternate")));
 #else
 #pragma weak CinternalAddUnitTestFunction=CinternalAddUnitTestFunction_alternate
 #pragma weak CinternalIterateAndCallUnitTestFunctions=CinternalIterateAndCallUnitTestFunctions_alternate
 #endif
 #endif
+
+
+CPPUTILS_DLL_PRIVATE void CallCinternalAddUnitTestFunction(TypeFunction a_function) {
+#ifdef HAS_ACTIVE_CALL
+    if (CinternalAddUnitTestFunction) {
+        CinternalAddUnitTestFunction(a_function);
+    }
+    else {
+        CinternalAddUnitTestFunction_alternate(a_function);
+    }
+#else
+    CinternalAddUnitTestFunction(a_function);
+#endif
+}
+
+
+static void CallCinternalIterateAndCallUnitTestFunctions(void) {
+#ifdef HAS_ACTIVE_CALL
+    if (CinternalIterateAndCallUnitTestFunctions) {
+        CinternalIterateAndCallUnitTestFunctions();
+    }
+    else {
+        CinternalIterateAndCallUnitTestFunctions_alternate();
+    }
+#else
+    CinternalIterateAndCallUnitTestFunctions();
+#endif
+}
 
 
 CPPUTILS_END_C
