821d3beb27b7611550a2a0e9539aa038e4ae1855
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-24 19:55:46 +0200

    checking unnamed semaphore 04

--- src/tests/unit_test/cinternal_unit_test_0003_unnamed_sema.c
+++ src/tests/unit_test/cinternal_unit_test_0003_unnamed_sema.c
@@ -7,7 +7,6 @@
 //
 
 #include <cinternal/unit_test_tools.h>
-#if 1
 #include <cinternal/unit_test.h>
 #define cinternal_unnamed_sema_wait_ms_needed
 #include <cinternal/unnamed_semaphore.h>
@@ -18,11 +17,8 @@
 CPPUTILS_UTEST_ALL(f_0003_unnamed_sema, t_0001) {
 
     cinternal_unnamed_sema_t aSema;
-    //CinternalUnitTestAssertCheck(cinternal_unnamed_sema_create(&aSema, 0)==0);
-    cinternal_unnamed_sema_create(&aSema, 0);
+    CinternalUnitTestAssertCheck(cinternal_unnamed_sema_create(&aSema, 0)==0);
+    //cinternal_unnamed_sema_create(&aSema, 0);
     cinternal_unnamed_sema_destroy(&aSema);
     CinternalUnitTestAssertCheck(true);
 }
-
-
-#endif
