bb4e94391f82396412a0289dcc600a08b95503ee
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-26 09:18:11 +0200

    modified unit test to make it more optimal

--- include/cinternal/unit_test.h
+++ include/cinternal/unit_test.h
@@ -10,9 +10,9 @@
 #define CUTILS_INCLUDE_CUTILS_UNIT_TEST_H
 
 #include <cinternal/export_symbols.h>
+#include <cinternal/unit_test_only_checks.h>
 #include <stdbool.h>
 
-#define CUTILS_UNIT_TEST_SPECIAL_SUBTEST_NUMBER		(-11)
 
 CPPUTILS_BEGIN_C
 
@@ -20,52 +20,50 @@ CPPUTILS_BEGIN_C
 #define CPPUTILS_CONDITIONAL_WEAKNESS		CPPUTILS_ONLY_GCCLIKE_ATTR_STRONG
 #endif
 
-typedef void (*TypeFunction)(void);
+typedef void (*TypeFunction)(const char* a_maj, const char* a_min);
 
-// if you need to have tests after main called, then overrwrite below 2 functions
-CPPUTILS_CONDITIONAL_WEAKNESS void CinternalAddUnitTestFunction(TypeFunction a_function);
+// one can overrwrite below 2 functions
+CPPUTILS_CONDITIONAL_WEAKNESS void CinternalAddUnitTestFunction(TypeFunction a_function, const char* a_maj, const char* a_min);
 CPPUTILS_CONDITIONAL_WEAKNESS void CinternalIterateAndCallUnitTestFunctions(void);
 
-CINTERNAL_EXPORT int  CinternalUnitTestCheckRawFn(bool a_condition, const char* a_cpcCondition, const char* a_testName, int a_subtestNumber, const char* a_cpcSrcPath, int a_line);
-CINTERNAL_EXPORT void CinternalUnitTestAssertCheckRawFn(bool a_condition, const char* a_cpcCondition, const char* a_testName, int a_subtestNumber, const char* a_cpcSrcPath, int a_line);
 
 #ifndef CPPUTILS_UNIT_TEST_NOEXCEPT
 #define CPPUTILS_UNIT_TEST_NOEXCEPT CPPUTILS_NOEXCEPT
 #endif
 
-#define CPPUTILS_UTEST_BEFORE_MAIN(_maj,_min)	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_bm_ ## _maj ## _min)
+#define CPPUTILS_UTEST_BEFORE_MAIN(_maj,_min)	\
+	static void cinternal_unit_test_bm_ ## _maj ## _min _raw(const char* a_maj, const char* a_min);	\
+	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_bm_ ## _maj ## _min){								\
+		cinternal_unit_test_bm_ ## _maj ## _min _raw(# _maj, # _min);								\
+	}																								\
+	static void cinternal_unit_test_bm_ ## _maj ## _min _raw(const char* CINTERNAL_UNIT_TEST_FN_ARG1_NAME, const char* CINTERNAL_UNIT_TEST_FN_ARG2_NAME)
 
-#define CPPUTILS_UTEST_AFTER_MAIN(_maj,_min)	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT;	\
-	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_adder_ ## _maj ## _min){						\
-		CinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min);					\
-	}																							\
-	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT
+#define CPPUTILS_UTEST_AFTER_MAIN(_maj,_min)	\
+	static void cinternal_unit_test_ ## _maj ## _min _raw (const char* a_maj, const char* a_min) CPPUTILS_UNIT_TEST_NOEXCEPT;	\
+	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_adder_ ## _maj ## _min){														\
+		CinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min _raw,# _maj, # _min);								\
+	}																															\
+	static void cinternal_unit_test_ ## _maj ## _min _raw (const char* CINTERNAL_UNIT_TEST_FN_ARG1_NAME, const char* CINTERNAL_UNIT_TEST_FN_ARG2_NAME) CPPUTILS_UNIT_TEST_NOEXCEPT
 
 
-#define CPPUTILS_UTEST_ALL(_maj,_min)	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT;\
-	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_adder_ ## _maj ## _min){						\
-		CinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min);					\
-		cinternal_unit_test_ ## _maj ## _min ();												\
-	}																							\
-	static void cinternal_unit_test_ ## _maj ## _min (void) CPPUTILS_UNIT_TEST_NOEXCEPT 
+#define CPPUTILS_UTEST_ALL(_maj,_min)	\
+	static void cinternal_unit_test_ ## _maj ## _min _raw (const char* a_maj, const char* a_min) CPPUTILS_UNIT_TEST_NOEXCEPT;	\
+	CPPUTILS_CODE_INITIALIZER(cinternal_unit_test_adder_ ## _maj ## _min){														\
+		CinternalAddUnitTestFunction(& cinternal_unit_test_ ## _maj ## _min _raw,# _maj, # _min);								\
+		cinternal_unit_test_ ## _maj ## _min _raw(# _maj, # _min);																\
+	}																															\
+	static void cinternal_unit_test_ ## _maj ## _min _raw (const char* CINTERNAL_UNIT_TEST_FN_ARG1_NAME, const char* CINTERNAL_UNIT_TEST_FN_ARG2_NAME) CPPUTILS_UNIT_TEST_NOEXCEPT 
 
 
 CPPUTILS_END_C
 
 
-#define CinternalUnitTestCheckRaw(_condition,_testName,_subtestNumber,_cpcSrcPath,_line)	\
-			CinternalUnitTestCheckRawFn((_condition)?true:false,#_condition,_testName,_subtestNumber,_cpcSrcPath,_line)
-#define CinternalUnitTestCheck(_condition)	CinternalUnitTestCheckRaw(_condition,"",CUTILS_UNIT_TEST_SPECIAL_SUBTEST_NUMBER,__FILE__,__LINE__)
-#define CinternalUnitTestAssertCheckRaw(_condition,_testName,_subtestNumber,_cpcSrcPath,_line)	\
-			CinternalUnitTestAssertCheckRawFn((_condition)?true:false,#_condition,_testName,_subtestNumber,_cpcSrcPath,_line)
-#define CinternalUnitTestAssertCheck(_condition)	CinternalUnitTestAssertCheckRaw(_condition,"",CUTILS_UNIT_TEST_SPECIAL_SUBTEST_NUMBER,__FILE__,__LINE__)
-
 #ifdef CINTERNAL_UNIT_TEST_USE_GTEST_LIKE_MACROSES
 #define TEST						CPPUTILS_UTEST_AFTER_MAIN
-#define EXPECT_EQ(_left,_right)		CinternalUnitTestCheck(((_left)==(_right)))
-#define ASSERT_EQ(_left,_right)		CinternalUnitTestAssertCheck(((_left)==(_right)))
-#define ASSERT_FALSE(_condition)	CinternalUnitTestAssertCheck((!(_condition)))
-#define ASSERT_TRUE					CinternalUnitTestAssertCheck
+#define EXPECT_EQ(_left,_right)		CinternalUnitTestCheckTst(((_left)==(_right)))
+#define ASSERT_EQ(_left,_right)		CinternalUnitTestAssertCheckTst(((_left)==(_right)))
+#define ASSERT_FALSE(_condition)	CinternalUnitTestAssertCheckTst((!(_condition)))
+#define ASSERT_TRUE					CinternalUnitTestAssertCheckTst
 #endif
 
 
---
+++ include/cinternal/unit_test_only_checks.h
@@ -0,0 +1,44 @@
+//
+// repo:			cinternal
+// file:            unit_test_only_checks.h
+// path:			include/cinternal/unit_test_only_checks.h
+// created on:		2023 Apr 15
+// created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#ifndef CUTILS_INCLUDE_CUTILS_UNIT_TEST_ONLY_CHECKS_H
+#define CUTILS_INCLUDE_CUTILS_UNIT_TEST_ONLY_CHECKS_H
+
+#include <cinternal/export_symbols.h>
+#include <stdbool.h>
+
+
+CPPUTILS_BEGIN_C
+
+
+#define CINTERNAL_UNIT_TEST_FN_ARG1_NAME		__a_testName
+#define CINTERNAL_UNIT_TEST_FN_ARG2_NAME		__a_subtestName
+
+CINTERNAL_EXPORT int  CinternalUnitTestCheckRawFn(bool a_condition, const char* a_cpcCondition, const char* a_testName, const char* a_subtestName, const char* a_cpcSrcPath, int a_line);
+CINTERNAL_EXPORT void CinternalUnitTestAssertCheckRawFn(bool a_condition, const char* a_cpcCondition, const char* a_testName, const char* a_subtestName, const char* a_cpcSrcPath, int a_line);
+
+#define CinternalUnitTestCheckRaw(_condition,_testName,_subtestName,_cpcSrcPath,_line)	\
+			CinternalUnitTestCheckRawFn((_condition)?true:false,#_condition,_testName,_subtestName,_cpcSrcPath,_line)
+#define CinternalUnitTestCheckSrc(_condition)	CinternalUnitTestCheckRaw(_condition,"","",__FILE__,__LINE__)
+#define CinternalUnitTestCheckTst(_condition)	CinternalUnitTestCheckRaw(_condition,CINTERNAL_UNIT_TEST_FN_ARG1_NAME,CINTERNAL_UNIT_TEST_FN_ARG2_NAME,"",-1)
+#define CinternalUnitTestCheckAll(_condition)	CinternalUnitTestCheckRaw(_condition,CINTERNAL_UNIT_TEST_FN_ARG1_NAME,CINTERNAL_UNIT_TEST_FN_ARG2_NAME,__FILE__,__LINE__)
+
+#define CinternalUnitTestAssertCheckRaw(_condition,_testName,_subtestName,_cpcSrcPath,_line)	\
+			CinternalUnitTestAssertCheckRawFn((_condition)?true:false,#_condition,_testName,_subtestName,_cpcSrcPath,_line)
+#define CinternalUnitTestAssertCheckSrc(_condition)	CinternalUnitTestAssertCheckRaw(_condition,"","",__FILE__,__LINE__)
+#define CinternalUnitTestAssertCheckTst(_condition)	CinternalUnitTestAssertCheckRaw(_condition,CINTERNAL_UNIT_TEST_FN_ARG1_NAME,CINTERNAL_UNIT_TEST_FN_ARG2_NAME,"",-1)
+#define CinternalUnitTestAssertCheckAll(_condition)	CinternalUnitTestAssertCheckRaw(_condition,CINTERNAL_UNIT_TEST_FN_ARG1_NAME,CINTERNAL_UNIT_TEST_FN_ARG2_NAME,__FILE__,__LINE__)
+
+
+extern CINTERNAL_EXPORT bool	g_bHasFailedTest;
+
+CPPUTILS_END_C
+
+
+
+#endif  // #ifndef CUTILS_INCLUDE_CUTILS_UNIT_TEST_ONLY_CHECKS_H
