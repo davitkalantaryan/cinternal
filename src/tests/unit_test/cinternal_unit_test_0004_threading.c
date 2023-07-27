06309287bd5fd2a583ad18a6cb6fcb822b3dac34
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-28 00:13:34 +0200

    sema tries are almost removed from threads test

--- src/tests/unit_test/cinternal_unit_test_0004_threading.c
+++ src/tests/unit_test/cinternal_unit_test_0004_threading.c
@@ -16,33 +16,33 @@
 #define CINTERNAL_THREAD_RET_CODE     ((cinternal_thread_ret_t)0)
 
 static cinternal_thread_ret_t CPPUTILS_THR_CALL ThreadFunction(void* a_pArg);
+#ifdef _WIN32  //  #ifdef _WIN32
+static cinternal_thread_ret_t CPPUTILS_THR_CALL ThreadFunctionOnlyWin(void* a_pArg);
+#endif
 
 
 CPPUTILS_UTEST_AFTER_MAIN(f_0004_threading, t_0001) {
 
     cinternal_thread_ret_t retCode;
     cinternal_thread_t aThread;
-    cinternal_unnamed_sema_t aSema;
-    CinternalUnitTestAssertCheckOpt(cinternal_unnamed_sema_create(&aSema, 0) == 0);
-    CinternalUnitTestAssertCheckOpt(cinternal_thread_create(&aThread, &ThreadFunction, &aSema)==0);
-    cinternal_unnamed_sema_post(&aSema);
+    CinternalUnitTestAssertCheckOpt(cinternal_thread_create(&aThread, &ThreadFunction, CPPUTILS_NULL)==0);
     cinternal_thread_wait_and_clean(&aThread,&retCode);
     CinternalUnitTestAssertCheckOpt(retCode== CINTERNAL_THREAD_RET_CODE);
 
     // now detaching
-    CinternalUnitTestAssertCheckOpt(cinternal_thread_create(&aThread, &ThreadFunction, &aSema) == 0);
-    cinternal_unnamed_sema_post(&aSema);
+    CinternalUnitTestAssertCheckOpt(cinternal_thread_create(&aThread, &ThreadFunction, CPPUTILS_NULL) == 0);
     cinternal_thread_detach(&aThread);
 
 #ifdef _WIN32  //  #ifdef _WIN32
     
     {
         DWORD dwId;
-        CinternalUnitTestAssertCheckOpt(cinternal_thread_create(&aThread, &ThreadFunction, &aSema) == 0);
+        cinternal_unnamed_sema_t aSema;
+        // we make trick with semas to be sure that when we try to attach to thread, thread is still alive
+        CinternalUnitTestAssertCheckOpt(cinternal_unnamed_sema_create(&aSema, 0) == 0);
+        CinternalUnitTestAssertCheckOpt(cinternal_thread_create(&aThread, &ThreadFunctionOnlyWin, &aSema) == 0);
         dwId = cinternal_thread_thread_id(aThread);
         cinternal_thread_detach(&aThread);
-        //printf("%d, cinternal_thread_attach(&aThread, dwId): %d\n", (int)dwId,(int)cinternal_thread_attach(&aThread, dwId));
-        //exit(1);
         CinternalUnitTestAssertCheckOpt(cinternal_thread_attach(&aThread, dwId)==0);
         cinternal_unnamed_sema_post(&aSema);
         cinternal_thread_wait(aThread);
@@ -59,7 +59,16 @@ CPPUTILS_UTEST_AFTER_MAIN(f_0004_threading, t_0001) {
 
 static cinternal_thread_ret_t CPPUTILS_THR_CALL ThreadFunction(void* a_pArg)
 {
+    CPPUTILS_STATIC_CAST(void, a_pArg);
+    cinternal_thread_exit_thread(CINTERNAL_THREAD_RET_CODE);
+}
+
+
+#ifdef _WIN32  //  #ifdef _WIN32
+static cinternal_thread_ret_t CPPUTILS_THR_CALL ThreadFunctionOnlyWin(void* a_pArg)
+{
     cinternal_unnamed_sema_t* pSema = (cinternal_unnamed_sema_t*)a_pArg;
     cinternal_unnamed_sema_wait(pSema);
     cinternal_thread_exit_thread(CINTERNAL_THREAD_RET_CODE);
 }
+#endif
