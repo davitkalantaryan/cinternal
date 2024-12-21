5e0e4234190c11a470330c3fd9934b5f89edf976
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-12-21 11:10:38 +0100

    some more ,acro concerning type handling

--- include/cinternal/typeinfo.h
+++ include/cinternal/typeinfo.h
@@ -11,6 +11,9 @@
 
 #include <cinternal/export_symbols.h>
 #include <cinternal/macroses.h>
+#include <cinternal/disable_compiler_warnings.h>
+#include <stddef.h>
+#include <cinternal/undisable_compiler_warnings.h>
 
 #define CPPUTILS_VAR_MACRO_FOR_TYPE(_x)     CPPUTILS_ID(_x)
 
@@ -26,13 +29,31 @@
         CPPUTILS_VAR_MACRO_APPY_COMMA(CPPUTILS_VAR_MACRO_FOR_TYPE,__VA_ARGS__)  \
     }
 
-//#define _MyMacro(type, name) name
-//#define MyMacro(_expr) _MyMacro _expr
-//
-//
-//#define CPPUTILS_STR_TEST(_type,_token)     CPPUTILS_ID(_type) CPPUTILS_ID(_token)
-//#define testMacro2(_type_and_token)         CPPUTILS_ID(_type_and_token)
-//#define testMacro3(...)                     CPPUTILS_VAR_MACRO_APPY_OP(testMacro2,;,__VA_ARGS__)
+#define CPPUTILS_ENUM_LIKE_DATA_NAMES(...)                  CPPUTILS_VAR_MACRO_APPY_COMMA(CPPUTILS_MACRO_NAME,__VA_ARGS__)
+
+
+#define CPPUTILS_DEFINE_VARIABLE_HEPLER(_type,_name)        CPPUTILS_ID(_type) CPPUTILS_ID(_name)
+#define CPPUTILS_DEFINE_VARIABLE(_expr)                     CPPUTILS_DEFINE_VARIABLE_HEPLER CPPUTILS_ID(_expr)
+
+#define CPPUTILS_VARIABLE_NAME_HEPLER(_type,_name)          CPPUTILS_ID(_name)
+#define CPPUTILS_VARIABLE_NAME(_expr)                       CPPUTILS_VARIABLE_NAME_HEPLER CPPUTILS_ID(_expr)
+#define CPPUTILS_VARIABLE_NAME_STR_HEPLER(_type,_name)      #_name
+#define CPPUTILS_VARIABLE_NAME_STR(_expr)                   CPPUTILS_VARIABLE_NAME_STR_HEPLER CPPUTILS_ID(_expr)
+
+#define CPPUTILS_VARIABLE_OFFSETOF(_strType,_expr)          ( (size_t)offsetof(CPPUTILS_ID(_strType),CPPUTILS_VARIABLE_NAME(_expr)) )
+
+#define CPPUTILS_SEMICOLON_TYPE_MEMBER_MCR_NM(_type,_name,...)  \
+    CPPUTILS_ID(_type) CPPUTILS_ID(_name) {  \
+        CPPUTILS_VAR_MACRO_APPY_OP(CPPUTILS_DEFINE_VARIABLE,;,__VA_ARGS__);  \
+    }
+
+#define CPPUTILS_SEMICOLON_TYPE_MEMBER_MCR(_type,...)  \
+    CPPUTILS_ID(_type) {  \
+        CPPUTILS_VAR_MACRO_APPY_OP(CPPUTILS_DEFINE_VARIABLE,;,__VA_ARGS__);  \
+    }
+
+#define CPPUTILS_STRUCT_LIKE_DATA_NAMES(...)                  CPPUTILS_VAR_MACRO_APPY_COMMA(CPPUTILS_VARIABLE_NAME_STR,__VA_ARGS__)
+
 
 
 CPPUTILS_BEGIN_C
--- src/tests/main_any_quick_test.cpp
+++ src/tests/main_any_quick_test.cpp
@@ -19,60 +19,26 @@
 
 typedef const char* ConstCharPtr;
 
-#define CPPUTILS_VAR_MACRO_FOR_ENUM(_x)     CPPUTILS_ID(_x)
-
-#define TEST_ENUM_MCR(_name,...)  \
-    enum {  \
-        CPPUTILS_VAR_MACRO_APPY_COMMA(CPPUTILS_VAR_MACRO_FOR_ENUM,__VA_ARGS__)  \
-    }
-
-#define TEST_ENUM_NAMES_MCR(...)  CPPUTILS_VAR_MACRO_APPY_COMMA(CPPUTILS_MACRO_NAME,__VA_ARGS__) 
-
 
 #define TEST_ENUM_FIELDS_MCR    \
     EnumNamefield1,             \
     EnumNamefield2 = 5
 
 #define TestStruct \
-    (int,field1)    \
-    (int,field2)
+    (int,structField1),    \
+    (int,structField2)
 
 
-TEST_ENUM_MCR(EnumName,TEST_ENUM_FIELDS_MCR);
-
-#define _MyMacro0(type, name) CPPUTILS_ID(type) CPPUTILS_ID(name)
-#define MyMacro0(_expr) _MyMacro0 CPPUTILS_ID(_expr)
-
-#define _MyMacro1(type, name) name
-#define MyMacro1(_expr) _MyMacro1 _expr
-
-#define _MyMacro2(type, name) offsetof(CPPUTILS_ID(type),CPPUTILS_ID(name))
-#define MyMacro2(_expr) _MyMacro2 _expr
-
-#define TestStructMacro(_name,...)  \
-    struct CPPUTILS_ID(_name) { \
-        CPPUTILS_VAR_MACRO_APPY_OP(MyMacro0,;,__VA_ARGS__)  \
-    }
-
-//#define CPPUTILS_VAR_MACRO_HELPER01(_x1,_x2,_op)            CPPUTILS_ID(_x1) CPPUTILS_ID(_op) CPPUTILS_ID(_x2)
-//#define CPPUTILS_VAR_MACRO_APPY_OP(_macro01,_op, ...)       CPPUTILS_VAR_MACRO_APPY_GEN(CPPUTILS_ID(_macro01),CPPUTILS_VAR_MACRO_HELPER01,_op,__VA_ARGS__)
-//
-//#define CPPUTILS_VAR_MACRO_HELPER02(_x1,_x2,_op)            CPPUTILS_ID(_x1) , CPPUTILS_ID(_x2)
-//#define CPPUTILS_VAR_MACRO_APPY_COMMA(_macro01, ...)        CPPUTILS_VAR_MACRO_APPY_GEN(CPPUTILS_ID(_macro01),CPPUTILS_VAR_MACRO_HELPER02, _macro01,__VA_ARGS__)
-
-//#define CPPUTILS_VAR_MACRO_HELPER02(_x1,_x2,_op)            CPPUTILS_ID(_x1) , CPPUTILS_ID(_x2)
-
-#define CPPUTILS_VAR_MACRO_HELPER04(_x1,_x2,_macro)            CPPUTILS_ID(_x1) , CPPUTILS_ID(_x2)
-#define CPPUTILS_VAR_MACRO_APPY_TEST(_macro01, ...)        CPPUTILS_VAR_MACRO_APPY_GEN(CPPUTILS_ID(_macro01),CPPUTILS_VAR_MACRO_HELPER02, _macro01,__VA_ARGS__)
-
-//TestStructMacro(StructName, TestStruct);
+CPPUTILS_COMMA_TYPE_MEMBER_MCR_NM(enum,EnumName,TEST_ENUM_FIELDS_MCR);
+CPPUTILS_SEMICOLON_TYPE_MEMBER_MCR_NM(struct,StructName, TestStruct);
 
 int main(void)
 {
     size_t i;
-    //int field1 = 5;
-    const ConstCharPtr enumNames[] = { TEST_ENUM_NAMES_MCR(TEST_ENUM_FIELDS_MCR)};
-    const size_t cunNamesSize = sizeof(enumNames) / sizeof(ConstCharPtr);
+    const ConstCharPtr enumNames[] = { CPPUTILS_ENUM_LIKE_DATA_NAMES(TEST_ENUM_FIELDS_MCR)};
+    const size_t cunEnumNamesSize = sizeof(enumNames) / sizeof(ConstCharPtr);
+    const ConstCharPtr structNames[] = { CPPUTILS_STRUCT_LIKE_DATA_NAMES(TestStruct) };
+    const size_t cunStructNamesSize = sizeof(structNames) / sizeof(ConstCharPtr);
 
     CPPUTILS_BISTATE_FLAGS_UN(field1, field2) aFlags;
     CPPUTILS_BISTATE_FLAGS_UN(field1, field2) aFlags4St;
@@ -93,10 +59,14 @@ int main(void)
     aFlags4St.wr.field1 = CPPUTILS_FOURSTATE_MAKE_BITS_ONGOING_FT;
     CInternalLogDebug("field1_true=%d, field2_true=%d", (int)aFlags4St.rd.field1_true, (int)aFlags4St.rd.field2_true);
 
-    CInternalLogDebug("SOME_ENUM1=%d, SOME_ENUM2=%d, cunNamesSize=%d", (int)EnumNamefield1, (int)EnumNamefield2,(int)cunNamesSize);
-
-    for (i = 0; i < cunNamesSize; ++i) {
+    CInternalLogDebug("SOME_ENUM1=%d, SOME_ENUM2=%d, cunEnumNamesSize=%d", (int)EnumNamefield1, (int)EnumNamefield2,(int)cunEnumNamesSize);
+    for (i = 0; i < cunEnumNamesSize; ++i) {
         CInternalLogDebug("%d => %s", (int)i, enumNames[i]);
+    }
+
+    CInternalLogDebug("cunStructNamesSize=%d", (int)cunStructNamesSize);
+    for (i = 0; i < cunStructNamesSize; ++i) {
+        CInternalLogDebug("%d => %s", (int)i, structNames[i]);
     }
 
     //CInternalLogDebug("%d", MyMacro((int,field1)));
