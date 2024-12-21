f131b2f25705cc930bd9f154bfd92de18b634772
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-12-21 15:05:31 +0100

    adeded CPPUTILS_ALL_NAMES macro

--- include/cinternal/macroses.h
+++ include/cinternal/macroses.h
@@ -10,7 +10,10 @@
 #define CINTERNAL_INCLUDE_CINTERNAL_MACROSES_H
 
 #include <cinternal/internal_header.h>
-#include <cinternal/macroses_variadic.h> 
+#include <cinternal/macroses_variadic.h>
+
+
+#define CPPUTILS_ALL_NAMES(...)			CPPUTILS_VAR_MACRO_APPY_COMMA(CPPUTILS_MACRO_NAME,__VA_ARGS__)
 
 
 #endif  // #ifndef CINTERNAL_INCLUDE_CINTERNAL_MACROSES_H
